6579460bd098303a5d19c33fc782febae0a06e43
rrrrrakane <enaka7788@gmail.com>
2021-09-14 14:05:24 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,10 @@
+
+public class Book extends TangibleAsset{
+	private String isbn;
+	public Book(String name, int priceStorin,String isbn){
+		supew
+			this.isbn = isbn
+	}
+pubor
+Book.java README.md TangibleAsset.java
+
